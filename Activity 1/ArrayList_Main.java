7fb2be6d74724d9a4541916a2cff53b890809c87
gin041420 <roginarolloque@gmail.com>
2022-09-30 21:39:44 +0800

    Added a files

---
+++ Activity 1/ArrayList_Main.java
@@ -0,0 +1,22 @@
+import java.util.*;  
+import java.util.ArrayList;
+public class ArrayList_Main {
+public static void main(String[] args)  
+ArrayList<String> STORE = new ArrayList();  
+Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+for (int i = 0; i < 5; i++){ 
+System.out.print("Enter first number- ");  
+int a= sc.nextInt();  
+System.out.print("Enter second number- ");  
+int b= sc.nextInt();  
+System.out.print("Enter third number- ");  
+int c= sc.nextInt();
+System.out.print("Enter fourth number- ");  
+int d= sc.nextInt();
+System.out.print("Enter fifth number- ");  
+int e= sc.nextInt();  
+}
+System.out.println(STORE); {
+STORE .add(scanner.nextLine());
+}  
+}  
