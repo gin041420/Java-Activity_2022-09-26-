67adee9f36eefd1c2f3ad9a18b59a727723ca2be
gin041420 <roginarolloque@gmail.com>
2022-10-12 12:48:01 +0800

    Added a Files

---
+++ Java Activity_Oct.-5-22/Music.java
@@ -0,0 +1,17 @@
+public class Music {
+    public String artist;
+    public String title;
+    
+    Music(String artist, String title){
+        this.artist = artist;
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
